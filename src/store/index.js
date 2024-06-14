aade1f9f0361ae44358b42f3a169ec7679e59d13
Jake Dusing <dusingjake@gmail.com>
2024-06-13 20:30:57 -0500

    update builder addCase

--- src/store/index.js
+++ src/store/index.js
@@ -35,7 +35,7 @@ const songsSlice = createSlice({
     },
   },
   extraReducers(builder) {
-    builder.addCase("movie/reset", (state, action) => {
+    builder.addCase(moviesSlice.actions.reset.toString(), (state, action) => {
       return [];
     });
   },
